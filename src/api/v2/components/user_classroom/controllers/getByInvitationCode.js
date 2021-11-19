234a5063a4aee03a5a8f6c2c0cedaa8d5a0c7675
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-19 21:34:45 +0700

    Updated getByInvitationCode controller of user_classroom - Send a status of 200 even if the user has already joined the classroom & add classroomId to the response

--- src/api/v2/components/user_classroom/controllers/getByInvitationCode.js
+++ src/api/v2/components/user_classroom/controllers/getByInvitationCode.js
@@ -3,6 +3,10 @@ import UserClassroom from '../model';
 import ROLES from '../constants/roles';
 
 const getByInvitationCode = async (req, res) => {
+  const responseObject = {
+    message: '',
+    classroomId: '',
+  };
   const { invitationCode } = req.params;
   const userId = req.user.id;
   try {
@@ -10,6 +14,7 @@ const getByInvitationCode = async (req, res) => {
     if (!classroom) {
       throw new Error('Invalid invitation code!');
     }
+    responseObject.classroomId = classroom._id;
     //   Check if this user has been enrolled to the classroom
     const ownedClassroom = await Classroom.findOne({
       _id: classroom._id,
@@ -20,18 +25,19 @@ const getByInvitationCode = async (req, res) => {
       classroomId: classroom._id,
     });
     if (ownedClassroom || existedUserClassroom) {
-      throw new Error('You have joined this classroom');
+      responseObject.message = 'You have already joined this classroom';
+    } else {
+      //   Create new user_classroom
+      const userClassroom = new UserClassroom({
+        userId,
+        classroomId: classroom._id,
+        role: ROLES.student,
+      });
+      await userClassroom.save();
+      responseObject.message = 'Successfully joined classroom';
     }
-    //   Create new user_classroom
-    const userClassroom = new UserClassroom({
-      userId,
-      classroomId: classroom._id,
-      role: ROLES.student,
-    });
-    await userClassroom.save();
-    res.status(200).send({
-      message: 'Successfully joined classroom',
-    });
+
+    res.status(200).send(responseObject);
   } catch (error) {
     res.status(400).send({
       message: error.message,
