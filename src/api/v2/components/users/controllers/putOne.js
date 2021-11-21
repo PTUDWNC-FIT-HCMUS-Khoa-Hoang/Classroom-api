18ae4ea9399e503d8b72de094d309f98d6d9c8c6
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-21 19:49:07 +0700

    Updated putOne controller (user) - Client has to provide currentPassword to update information

--- src/api/v2/components/users/controllers/putOne.js
+++ src/api/v2/components/users/controllers/putOne.js
@@ -1,8 +1,11 @@
 import User from '../model';
+import bcrypt from 'bcrypt';
+
+const CURRENT_PASSWORD_NOT_MATCHED_ERROR = 'Current password is not matched';
 
 const putOne = async (req, res) => {
   const userId = req.user.id;
-  const { studentId } = req.body;
+  const { studentId, currentPassword } = req.body;
   try {
     if (studentId) {
       const student = await User.findOne({ studentId });
@@ -10,9 +13,25 @@ const putOne = async (req, res) => {
         throw new Error('Existed student ID!');
       }
     }
-    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
-      new: true,
-    });
+
+    // Check current password
+    if (!currentPassword) {
+      throw new Error(CURRENT_PASSWORD_NOT_MATCHED_ERROR);
+    }
+    const userFoundById = await User.findById(userId);
+    const isPasswordValid = bcrypt.compareSync(
+      currentPassword,
+      userFoundById.password
+    );
+    if (!isPasswordValid) {
+      throw new Error(CURRENT_PASSWORD_NOT_MATCHED_ERROR);
+    }
+    // Update user
+    const user = await User.findById(userId);
+    user.fullname = req.body.fullname;
+    user.password = req.body.password || user.password;
+    user.studentId = req.body.studentId;
+    const updatedUser = await user.save();
     res.send(updatedUser);
   } catch (error) {
     res.status(400).send({
