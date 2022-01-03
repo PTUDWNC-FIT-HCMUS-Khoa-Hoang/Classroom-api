71aafe0458144fe2ebf1abb91f519825590ee78a
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-03 14:51:47 +0700

    Add timestamp to user model

--- src/api/v2/components/users/model.js
+++ src/api/v2/components/users/model.js
@@ -4,38 +4,43 @@ import jwt from 'jsonwebtoken';
 
 const SALT_ROUNDS = 10;
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    index: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    studentId: {
+      type: String,
+      index: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    secretOtp: {
+      type: String,
+    },
+    googleId: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  studentId: {
-    type: String,
-    index: true,
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  secretOtp: {
-    type: String,
-  },
-  googleId: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.toJSON = function () {
   const userObject = this.toObject();
