a5152b91c315cf2940c29a0a247a5f150d5e3929
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-17 23:53:58 +0700

    Classroom service - putOneById now has a step of finalizeGrade to check if teacher finalizes a grade column then do some work

--- src/api/v2/components/classrooms/services/putOneById.js
+++ src/api/v2/components/classrooms/services/putOneById.js
@@ -1,6 +1,13 @@
 import Classroom from '../model';
+import finalizeGrade from './finalizeGrade';
 
-const putOneById = async (classroomId, updateData) => {
+const putOneById = async (updater, classroomId, updateData) => {
+  // Check if this is grade finalization
+  const keys = Object.keys(updateData);
+  if (keys.includes('gradeStructure')) {
+    await finalizeGrade(classroomId, updateData.gradeStructure, updater);
+  }
+
   const updatedClassroom = await Classroom.findByIdAndUpdate(
     classroomId,
     updateData,
