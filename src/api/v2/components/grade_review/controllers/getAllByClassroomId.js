eb33157c1114cf9173306908b1aef267c2c0d1ec
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-18 14:04:50 +0700

    Grade Review - getAllByClassroomId controller update: only allow teacher & owner

--- src/api/v2/components/grade_review/controllers/getAllByClassroomId.js
+++ src/api/v2/components/grade_review/controllers/getAllByClassroomId.js
@@ -1,10 +1,18 @@
 import gradeDetailServices from '../../grade_detail/services';
 import gradeReviewServices from '../services';
+import checkRole from '../../classrooms/helpers/checkRole';
+import ROLES from '../../../constants/role';
 
 const getAllByClassroomId = async (req, res) => {
   const { classroomId } = req.params;
 
   try {
+    await checkRole({
+      userId: req.user.id,
+      classroomId,
+      roles: [ROLES.TEACHER],
+    });
+
     const gradeDetails = await gradeDetailServices.getByClassroomId(
       classroomId
     );
