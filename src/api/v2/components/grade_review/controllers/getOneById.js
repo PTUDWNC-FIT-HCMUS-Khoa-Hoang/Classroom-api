46947d3852c1dcdde3ef031499c7a34fb1c4c74c
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-18 20:27:30 +0700

    Grade Review - getOneById controller: add field 'gradeComposition' to response

--- src/api/v2/components/grade_review/controllers/getOneById.js
+++ src/api/v2/components/grade_review/controllers/getOneById.js
@@ -1,6 +1,7 @@
 import ROLES from '../../../constants/role';
 import checkRole from '../../classrooms/helpers/checkRole';
 import gradeDetailServices from '../../grade_detail/services';
+import classroomServices from '../../classrooms/services';
 import gradeReviewServices from '../services';
 
 const getOneById = async (req, res) => {
@@ -14,6 +15,13 @@ const getOneById = async (req, res) => {
     const gradeDetail = await gradeDetailServices.getOneById(
       gradeReview.gradeDetail
     );
+
+    const classroom = await classroomServices.getOneById(
+      gradeDetail.classroomId
+    );
+    const gradeComposition = classroom.gradeStructure.find(
+      (grade) => grade._id.toString() === gradeDetail.gradeId.toString()
+    );
     await checkRole({
       classroomId: gradeDetail.classroomId,
       userId: req.user.id,
@@ -23,6 +31,7 @@ const getOneById = async (req, res) => {
     res.status(200).send({
       ...gradeReview._doc,
       gradeDetail,
+      gradeComposition,
     });
   } catch (error) {
     res.status(400).send({
